ec140d9ec35d67e43605878b6344b45d4438aa2b
we.njing666@163.com <we.njing666@163.com>
2023-05-12 21:55:11 +0800

    update

--- src/java/org/apache/cassandra/audit/es/CassandraUtil.java
+++ src/java/org/apache/cassandra/audit/es/CassandraUtil.java
@@ -20,6 +20,8 @@ package org.apache.cassandra.audit.es;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CassandraUtil {
 
@@ -31,5 +33,40 @@ public class CassandraUtil {
 
     public static void getTableParams(String tableName,Boolean syncEs){
         syncTablesInfo.put(tableName,syncEs);
+    }
+
+    public static String matchSqlTableName(String sql){
+        Matcher matcher = null;
+        //SELECT 列名称 FROM 表名称
+        //SELECT * FROM 表名称
+        if( sql.startsWith("select") ){
+            matcher = Pattern.compile("select\\s.+from\\s(.+)where\\s(.*)").matcher(sql);
+            if(matcher.find()){
+                return matcher.group(1).trim();
+            }
+        }
+        //INSERT INTO 表名称 VALUES (值1, 值2,....)
+        //INSERT INTO table_name (列1, 列2,...) VALUES (值1, 值2,....)
+        if( sql.startsWith("insert") ){
+            matcher = Pattern.compile("insert\\sinto\\s(.+)\\(.*\\)\\s.*").matcher(sql);
+            if(matcher.find()){
+                return matcher.group(1).trim();
+            }
+        }
+        //UPDATE 表名称 SET 列名称 = 新值 WHERE 列名称 = 某值
+        if( sql.startsWith("update") ){
+            matcher = Pattern.compile("update\\s(.+)set\\s.*").matcher(sql);
+            if(matcher.find()){
+                return matcher.group(1).trim();
+            }
+        }
+        //DELETE FROM 表名称 WHERE 列名称 = 值
+        if( sql.startsWith("delete") ){
+            matcher = Pattern.compile("delete\\sfrom\\s(.+)where\\s(.*)").matcher(sql);
+            if(matcher.find()){
+                return matcher.group(1).trim();
+            }
+        }
+        return null;
     }
 }
