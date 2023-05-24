91d1a19927a021acfe63085d5488874aefc89448
we.njing666@163.com <we.njing666@163.com>
2023-05-24 22:13:51 +0800

    update

--- src/java/org/apache/cassandra/audit/es/EsUtil.java
+++ src/java/org/apache/cassandra/audit/es/EsUtil.java
@@ -111,7 +111,7 @@ public class EsUtil {
             if (i > 0){
                 sb.append(",");
             }
-            sb.append("\""+key+"\":\""+value.replaceAll("&&",",").replaceAll("\\{","(").replaceAll("\\)","}")+"\"");
+            sb.append("\""+key+"\":\""+value.replaceAll("&&",",")+"\"");
             i++;
         }
         sb.append("}");
